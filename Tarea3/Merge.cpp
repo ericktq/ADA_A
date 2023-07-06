5c6b747b385fde35574225810ed4811b6cdf95b5
ericktq <64106507+ericktq@users.noreply.github.com>
2023-07-05 23:08:05 -0500

    Merge.cpp

--- Tarea3/Merge.cpp
+++ Tarea3/Merge.cpp
@@ -1,109 +1,83 @@
-// C++ program to merge K
-// sorted arrays
- 
-#include<bits/stdc++.h>
-#define n 4
- 
+
+#include <bits/stdc++.h>
 using namespace std;
- 
-// Function to perform merge operation
-void merge(int l, int r, vector<int>& output)
+
+void merge(int array[], int const L, int const medio,int const R)
 {
-    // to store the starting point
-    // of left and right array
-    int l_in = l * n, r_in
-                      = ((l + r) / 2 + 1) * n;
+    int const array1 = medio - L + 1;
+    int const array2 = R - medio;
  
-    // To store the size of left and
-    // right array
-    int l_c = ((l + r) / 2 - l + 1) * n;
-    int r_c = (r - (l + r) / 2) * n;
+    auto *LArray = new int[array1],
+         *RArray = new int[array2];
  
-    // array to temporarily store left
-    // and right array
-    int l_arr[l_c], r_arr[r_c];
+    for (auto i = 0; i < array1; i++)
+        LArray[i] = array[L + i];
+    for (auto j = 0; j < array2; j++)
+        RArray[j] = array[medio + 1 + j];
  
-    // storing data in left array
-    for (int i = 0; i < l_c; i++)
-        l_arr[i] = output[l_in + i];
+    auto indexOfarray1 = 0, indexOfarray2 = 0;
+    int indexOfMergedArray = L;
  
-    // storing data in right array
-    for (int i = 0; i < r_c; i++)
-        r_arr[i] = output[r_in + i];
- 
-    // to store the current index of
-    // temporary left and right array
-    int l_curr = 0, r_curr = 0;
- 
-    // to store the current index
-    // for output array
-    int in = l_in;
- 
-    // two pointer merge for
-    // two sorted arrays
-    while (
-        l_curr + r_curr < l_c + r_c) {
-        if (
-            r_curr == r_c
-            || (l_curr != l_c
-                && l_arr[l_curr] < r_arr[r_curr]))
-            output[in]
-                = l_arr[l_curr],
-                l_curr++, in++;
-        else
-            output[in]
-                = r_arr[r_curr],
-                r_curr++, in++;
+    while (indexOfarray1 < array1
+           && indexOfarray2 < array2) {
+        if (LArray[indexOfarray1]
+            <= RArray[indexOfarray2]) {
+            array[indexOfMergedArray]
+                = LArray[indexOfarray1];
+            indexOfarray1++;
+        }
+        else {
+            array[indexOfMergedArray]
+                = RArray[indexOfarray2];
+            indexOfarray2++;
+        }
+        indexOfMergedArray++;
     }
+ 
+    while (indexOfarray1 < array1) {
+        array[indexOfMergedArray]
+            = LArray[indexOfarray1];
+        indexOfarray1++;
+        indexOfMergedArray++;
+    }
+ 
+    while (indexOfarray2 < array2) {
+        array[indexOfMergedArray]
+            = RArray[indexOfarray2];
+        indexOfarray2++;
+        indexOfMergedArray++;
+    }
+    delete[] LArray;
+    delete[] RArray;
 }
- 
-// Code to drive merge-sort and
-// create recursion tree
-void divide(int l, int r, vector<int>& output,
-            vector<vector<int>> arr)
+
+void mergeSort(int array[], int const begin, int const end)
 {
-    if (l == r) {
- 
-        /* base step to initialize the output
-           array before performing merge
-           operation */
-        for (int i = 0; i < n; i++)
-            output[l * n + i] = arr[l][i];
- 
+    if (begin >= end)
         return;
-    }
  
-    // To sort left half
-    divide(l, (l + r) / 2,
-           output, arr);
- 
-    // To sort right half
-    divide((l + r) / 2 + 1, r,
-           output, arr);
- 
-    // Merge the left and right half
-    merge(l, r, output);
+    int medio = begin + (end - begin) / 2;
+    mergeSort(array, begin, medio);
+    mergeSort(array, medio + 1, end);
+    merge(array, begin, medio, end);
 }
  
-// Driver Function
+void printArray(int A[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << A[i] << " ";
+    cout << endl;
+}
+
 int main()
 {
-    // input 2D-array
-    vector<vector<int>> arr = { { 5, 7, 15, 18 },
-                     { 1, 8, 9, 17 },
-                     { 1, 4, 7, 7 } };
- 
-    // Number of arrays
-    int k = arr.size();
- 
-    // Output array
-    vector<int> output(n*k);
- 
-    divide(0, k - 1, output, arr);
- 
-    // Print merged array
-    for (int i = 0; i < n * k; i++)
-        cout << output[i] << " ";
- 
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]); 
+    cout << "Elementos \n";
+    printArray(arr, arr_size); 
+    mergeSort(arr, 0, arr_size - 1); 
+    cout << "\nArreglo ordenado \n";
+    printArray(arr, arr_size);
     return 0;
 }
+ 
