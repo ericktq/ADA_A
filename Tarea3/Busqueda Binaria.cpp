0303f8ec4d7846417503f816d0a625a14ea4c787
ericktq <64106507+ericktq@users.noreply.github.com>
2023-07-05 22:53:14 -0500

    Update Busqueda Binaria.cpp

--- Tarea3/Busqueda Binaria.cpp
+++ Tarea3/Busqueda Binaria.cpp
@@ -1,10 +1,5 @@
-//Busqueda Binaria
-
 #include<iostream>
-#include<conio.h>
-
 using namespace std;
-
 int main(){
 	int numeros[] = {1,2,3,4,5};
 	int inf,sup,mitad,dato,i;
@@ -39,7 +34,6 @@ int main(){
 	else{
 		cout<<"El numero NO se encontro";
 	}
-	
-	getch();
+
 	return 0;
 }
